a7b71f841b6274792e19a4f449563cf84b15ecec
yusuke-miura <miura2000T@outlook.jp>
2023-04-26 21:19:01 +0900

    feat(index):ejs読み込めるようにした

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require("express");
 const app = express()
 const port = 3000
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.get('/zoom', (req, res) => {
+    res.render('zoom');
+});
 
 app.listen(port, () => {
     console.log('listening on *:3000');
