6a877c42227de0ebf02f547c78e3f4804f72932f
yusuke-miura <miura2000T@outlook.jp>
2023-04-29 19:19:06 +0900

    refact(index)：コメントの追加

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
+
 const port = 3000
 
+// ejsをexpressで利用できるようにejsを指定
 app.set('view engine', 'ejs');
+
+// expressサーバからpublicフォルダにアクセスできるようにする
 app.use(express.static('public'));
 
+// ルーティング処理
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
@@ -15,10 +20,12 @@ app.get('/zoom', (req, res) => {
     res.render('zoom');
 });
 
+// connectionイベント(ブラウザからアクセス)があったときの処理
 io.on('connection', (socket => {
     console.log('ユーザが接続しました');
 }))
 
+// port3000番号で起動
 app.listen(port, () => {
     console.log('listening on *:3000');
 })
