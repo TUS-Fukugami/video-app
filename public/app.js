336425688543b975d275b81fc8f3c0538224408f
DanielClemKim <6323513@ed.tus.ac.jp>
2023-04-30 19:26:10 +0900

    feat(vue): vue setting, create sending message method

--- public/app.js
+++ public/app.js
@@ -1,2 +1,22 @@
+// サーバと接続開始
 const socket = io();
-console.log(window.location);
+
+const app = Vue.createApp({
+  data() {
+    message: "";
+  },
+  methods: {
+    // message送信のメソッド
+    sendMessage() {
+      // サーバへmessageを送信
+      socket.emit("message", this.message);
+      this.message = "";
+    },
+  },
+  mounted() {
+    // サーバから送信されたmessageを受け取る
+    socket.on("message", (msg) => {
+      console.log("message: " + msg);
+    });
+  },
+});
